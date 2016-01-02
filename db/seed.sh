a8930395126ea0e2ef832cee858aa9f7b39f6b2e
briptimus <brituor@gmail.com>
2016-01-02 13:53:40 -0800

    Reverted dependencies on package.json to the ones in the book

--- db/seed.sh
+++ db/seed.sh
@@ -1,2 +1,3 @@
+/c/"Program Files"/MongoDB/Server/3.0/bin/mongo blog --eval "db.dropDatabase()"
 /c/"Program Files"/MongoDB/Server/3.0/bin/mongoimport --db blog --collection users --file ./db/users.json --jsonArray
 /c/"Program Files"/MongoDB/Server/3.0/bin/mongoimport --db blog --collection articles --file ./db/articles.json --jsonArray
