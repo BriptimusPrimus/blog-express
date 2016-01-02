1055afa66813c12f704a3705a393118336a033c3
briptimus <brituor@gmail.com>
2016-01-02 12:55:37 -0800

    DB Seed script

--- db/seed.sh
+++ db/seed.sh
@@ -1,2 +1,2 @@
-mongoimport --db blog --collection users --file ./db/users.json --jsonArray
-mongoimport --db blog --collection articles --file ./db/articles.json --jsonArray
+/c/"Program Files"/MongoDB/Server/3.0/bin/mongoimport --db blog --collection users --file ./db/users.json --jsonArray
+/c/"Program Files"/MongoDB/Server/3.0/bin/mongoimport --db blog --collection articles --file ./db/articles.json --jsonArray
